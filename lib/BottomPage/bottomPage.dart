220bcb36fa007480993a8ccca295927645e7a455
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-28 17:47:34 +0530

    material button is added

--- lib/BottomPage/bottomPage.dart
+++ lib/BottomPage/bottomPage.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
 
 class BottomPage extends StatelessWidget {
+  List<Widget> pageChildren() {
+    return <Widget>[
+      Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "Simple Desktop \nApplication",
+              style: TextStyle(
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Text(
+                'Desktop support allows you to compile Flutter source code to a native Windows, macOS, or Linux desktop app.',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            MaterialButton(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20.0),
+                ),
+              ), onPressed: () {  },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal: 40.0),
+                child: Text("Get Started",style: TextStyle(
+                  color: Colors.blue[600],
+                  fontSize: 23,
+                  fontWeight: FontWeight.bold,
 
-List<Widget> pageChildren() {
-
-  return <Widget>[
-    Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("Simple Desktop \nApplication",style: TextStyle(
-           fontSize: 40.0,
-           fontWeight: FontWeight.bold,
-           color: Colors.white,
-          ),
-          ),
-          Text('Desktop support allows you to compile Flutter source code to a native Windows, macOS, or Linux desktop app.',style: TextStyle(
-           fontSize: 16.0,
-           color: Colors.white,
-          ),
-          ),
-          
-        ],
+                ),),
+              ),
+            ),
+            
+          ],
+        ),
+        
       ),
-    ),
-  ];
-}
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +58,6 @@ List<Widget> pageChildren() {
           return Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: pageChildren(),
-
-            
           );
         } else {
           return Column(
