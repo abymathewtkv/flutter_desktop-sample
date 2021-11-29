61463f229ccc7efc4bc1583f87072fafc1fa2216
Aby T Mathew <abymathewtkv@gmail.com>
2021-11-29 16:28:38 +0530

    login page is completed with validation

--- lib/LoginPage/loginPage.dart
+++ lib/LoginPage/loginPage.dart
@@ -6,11 +6,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+
+final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+    
       appBar: AppBar(
-       
         flexibleSpace: Container(
           decoration: BoxDecoration(
               gradient: LinearGradient(
@@ -38,66 +42,99 @@ class _LoginPageState extends State<LoginPage> {
         child: Center(
           child: Container(
             width: 350,
-            height: 350,
+            height: 400,
             child: Card(
               elevation: 5.0,
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Form(
+                  key: _formKey,
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
                       Padding(
                         padding: const EdgeInsets.all(0.0),
                         child: Container(
-                          child: Column(
-                            children: <Widget>[
-                              Image.asset("assets/images/windows1.png",height: 80,width: 80,),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              TextFormField(
-                                decoration: InputDecoration(
-                                  prefixIcon : Icon(Icons.person),
-                                  hintText: 'Username',
-                                  focusColor: Colors.blue[700],
-                               contentPadding: EdgeInsets.only(left: 20,right: 20),
-                                  border: OutlineInputBorder(),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                    color: Colors.blue,
-                                    width: 2,
-                                  )),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset(
+                                  "assets/images/windows1.png",
+                                  height: 80,
+                                  width: 80,
                                 ),
-                              ),
-                              SizedBox(
-                                height: 20.0,
-                              ),
-                              TextFormField(
-                                obscureText: true,
-                                decoration: InputDecoration(
-                                  prefixIcon :Icon(Icons.vpn_key_rounded),
-                                  
-                                  hintText: 'Password',
-                                  focusColor: Colors.blue[700],
-                                  contentPadding: EdgeInsets.only(left: 20,right: 20),
-                                  border: OutlineInputBorder(),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                    color: Colors.blue,
-                                    width: 2,
-                                  )),
+                                SizedBox(
+                                  height: 20,
                                 ),
-                              ),
-                            ],
+                                TextFormField(
+                                  validator: (value) {
+                                    if(value!.isEmpty){
+                                      return "Enter Username";
+                                    }
+                                    return null;
+                                  },
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(Icons.person),
+                                    hintText: 'Username',
+                                    focusColor: Colors.blue[700],
+                                    contentPadding:
+                                        EdgeInsets.only(left: 20, right: 20),
+                                    border: OutlineInputBorder(),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                      color: Colors.blue,
+                                      width: 2,
+                                    )),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 20.0,
+                                ),
+                                TextFormField(
+                                  validator: (value) {
+                                    if(value!.isEmpty){
+                                      return "Enter Password";
+                                    }
+                                    if(value.length<6){
+                                      return "Minimu 6 Characters required";
+                                    }
+                                    return null;
+                                  },
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    prefixIcon: Icon(Icons.vpn_key_rounded),
+                                    hintText: 'Password',
+                                    focusColor: Colors.blue[700],
+                                    contentPadding:
+                                        EdgeInsets.only(left: 20, right: 20),
+                                    border: OutlineInputBorder(),
+                                    focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                      color: Colors.blue,
+                                      width: 2,
+                                    )),
+                                  ),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                          child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(fontSize: 12, color: Colors.blue),
+                      )),
                       Row(
                         children: [
                           Expanded(
                             child: FlatButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                if(_formKey.currentState!.validate()){
+                                  print("valilated");
+                                }
+                              },
                               child: Text(
                                 'Login',
                                 style: TextStyle(
